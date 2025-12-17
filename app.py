7166889ecff7935686384fc856220a92e2dbfc8d
jake l school acct <239911@shakopeeschools.org>
2025-12-16 18:54:47 -0600

    i gotta hurry up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import subprocess
 
 app = Flask(__name__)
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/run', methods=['POST'])
+def run_tool():
+    tool = request.form.get('tool')
+    target = request.form.get('target')
+    
+    if not target:
+        return jsonify({'error': 'please enter domain or ip :3'}), 400
+    
+    commands = {
+        'ping': ['ping', '-c', '4', target],
+        'nslookup': ['nslookup', target],
+        'traceroute': ['traceroute', target],
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
